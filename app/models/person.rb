0d358494ca846fdf3645c9898594464be844e0cc
Sirajuddin Maizir <siroj100@gmail.com>
2010-09-08 10:51:26 +0700

    added person and address model

---
+++ app/models/person.rb
@@ -0,0 +1,30 @@
+class Person < ActiveRecord::Base
+
+  hobo_model # Don't put anything above this
+
+  fields do
+    name :string
+    sex  :string
+    timestamps
+  end
+
+
+  # --- Permissions --- #
+
+  def create_permitted?
+    acting_user.administrator?
+  end
+
+  def update_permitted?
+    acting_user.administrator?
+  end
+
+  def destroy_permitted?
+    acting_user.administrator?
+  end
+
+  def view_permitted?(field)
+    true
+  end
+
+end
