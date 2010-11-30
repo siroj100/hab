6c982267538ec92b0515954ba2048bec4da88116
Umur Ozkul <umur.ozkul@gmail.com>
2010-11-30 14:37:54 +0100

    Moved login_as to support/

--- spec/acceptance/support/helpers.rb
+++ spec/acceptance/support/helpers.rb
@@ -4,7 +4,14 @@ module HelperMethods
   def logger
     Rails::logger
   end
-  
+
+  def login_as(user)
+    visit '/login'
+    fill_in 'login', :with => user.email_address
+    fill_in 'password', :with => user.password
+    click_button 'Log in'
+  end
+
 end
 
 Spec::Runner.configuration.include(HelperMethods)
